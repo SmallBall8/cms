38b95d270a41ba88b2c62183ff6029fa0d0b8598
SmallBall8 <61188425+SmallBall8@users.noreply.github.com>
2023-05-09 15:43:36 +0800

    Delete day3 directory

--- day3/src/global/constants.ts
+++
@@ -1,2 +0,0 @@
-//专门用来定义常量的
-export const LOGIN_TOKEN = 'login/token'
--- day3/src/types/login.ts
+++
@@ -1,9 +0,0 @@
-//专门用来放类型定义
-export interface IAccount {
-  name: string
-  password: string
-}
-export interface IPhone {
-  number: string
-  password: string
-}
